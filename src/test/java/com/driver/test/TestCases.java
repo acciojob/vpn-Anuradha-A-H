601d016f09efedee8fa25de5dd73c19ae590f4d9
Anuradha <anuradhaah667@gmail.com>
2024-05-04 15:28:52 +0530

    testcase

--- src/test/java/com/driver/test/TestCases.java
+++ src/test/java/com/driver/test/TestCases.java
@@ -1,23 +1,38 @@
 package com.driver.test;
 
-import com.driver.model.*;
-import com.driver.repository.*;
-import com.driver.services.UserService;
-import com.driver.services.impl.*;
-import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
+
+import com.driver.model.Admin;
+import com.driver.services.impl.AdminServiceImpl;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+
 
 @RunWith(MockitoJUnitRunner.Silent.class)
+@SpringBootTest
 public class TestCases {
 
+    private final AdminServiceImpl adminService;
+
+    @Autowired
+    public TestCases(AdminServiceImpl adminService) {
+        this.adminService = adminService;
+    }
+
+    @Test
+    public void testRegister() {
+        // Call the register method with some dummy values
+        Admin admin = adminService.register("admin23232", "admin123");
+
+        // Assert that the returned admin is not null
+        assertNotNull(admin);
+    }
 }
+
+
 
